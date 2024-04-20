7c67bef2f91becc6e211a181a4683b1eb7ba0d5b
it sps <fhaesolaa01@gmail.com>
2024-04-20 08:13:28 +0700

    update 4-20-2024

--- laravel/resources/views/dashboard/holding.blade.php
+++ laravel/resources/views/dashboard/holding.blade.php
@@ -1,4 +1,93 @@
 @extends('layouts.holding')
 @section('isi')
+  <!-- ======= Top Bar ======= -->
+<section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+        <div class="contact-info d-flex align-items-center">
+        </div>
+    </div>
+</section>
+  <!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center" style="height: 100%">
+    <div class="container" data-aos="zoom-out" data-aos-delay="100">
+        <div class="row">
+            <!-- <div class="col-3"> -->
+            <h1>Welcome to <span>APP HRD</span></h1>
+            <h2>We are team of talented designers making websites with Bootstrap</h2>
+            <div class="d-flex">
+                <a href="#" onclick="event.preventDefault();document.getElementById('logout-form').submit();" class="btn-get-started scrollto">Logout</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+	                {{ csrf_field() }}
+	            </form>
+            </div>
+            <!-- </div> -->
+            <div class="col-12">
+                <section id="featured-services" class="featured-services">
+                    <div class="container" data-aos="fade-up">
+                        <div class="row">
+                            <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
+                                <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+                                    <div class="row">
+                                        <a href="{{ url('dashboard/holding/sp') }}">
+                                            <div class="col-12">
+                                                <img src="{{ url('public/holding/assets/img/logosp.png') }}" width="50%" style="margin-right: 25%;margin-left: 25%">
+                                            </div>
+                                            <h4 class="title" style="text-align: center;"><a href="">CV. SUMBER PANGAN</a></h4>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
+                                <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
+                                    <div class="row">
+                                        <a href="{{ url('dashboard/holding/sps') }}">
+                                            <div class="col-12">
+                                                <img src="{{ url('public/holding/assets/img/logosps.png') }}" width="50%" style="margin-right: 25%;margin-left: 25%">
+                                            </div>
+                                            <h4 class="title" style="text-align: center;"><a href="">PT. SUMBER PANGAN</a></h4>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
+                                <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
+                                    <div class="row">
+                                        <a href="{{ url('dashboard/holding/sip') }}">
+                                            <div class="col-12">
+                                                <img src="{{ url('public/holding/assets/img/logosip.png') }}" width="50%" style="margin-right: 25%;margin-left: 25%">
+                                            </div>
+                                            <h4 class="title" style="text-align: center;"><a href="">CV. SURYA INTI PANGAN</a></h4>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+        </div>
+  </section><!-- End Hero -->
 
+<footer id="footer">
+    <div class="container py-4">
+        <div class="copyright">
+            &copy; Copyright <strong><span>BizLand</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bizland-bootstrap-business-template/ -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+    </div>
+</footer><!-- End Footer -->
+
+<section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+        <div class="contact-info d-flex align-items-center">
+        </div>
+    </div>
+</section>
 @endsection
--- laravel/resources/views/layouts/holding.blade.php
+++ laravel/resources/views/layouts/holding.blade.php
@@ -10,22 +10,22 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ url('public/assets/img/favicon.png') }}" rel="icon">
-  <link href="{{ url('public/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+  <link href="{{ url('public/holding/assets/img/favicon.png') }}" rel="icon">
+  <link href="{{ url('public/holding/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{ url('public/assets/vendor/aos/aos.css') }}" rel="stylesheet">
-  <link href="{{ url('public/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ url('public/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ url('public/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-  <link href="{{ url('public/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-  <link href="{{ url('public/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+  <link href="{{ url('public/holding/assets/vendor/aos/aos.css') }}" rel="stylesheet">
+  <link href="{{ url('public/holding/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ url('public/holding/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ url('public/holding/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{ url('public/holding/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ url('public/holding/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="{{ url('public/assets/css/style.css') }}" rel="stylesheet">
+  <link href="{{ url('public/holding/assets/css/style.css') }}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: BizLand
@@ -38,109 +38,21 @@
 
 <body>
 
-  <!-- ======= Top Bar ======= -->
-  <section id="topbar" class="d-flex align-items-center">
-    <div class="container d-flex justify-content-center justify-content-md-between">
-      <div class="contact-info d-flex align-items-center">
-      </div>
-    </div>
-  </section>
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center" style="height: 100%">
-    <div class="container" data-aos="zoom-out" data-aos-delay="100">
-      <div class="row" style="margin-top:10%">
-        <!-- <div class="col-3"> -->
-        <h1>Welcome to <span>APP HRD</span></h1>
-        <h2>We are team of talented designers making websites with Bootstrap</h2>
-        <div class="d-flex">
-          <a href="#about" class="btn-get-started scrollto">Get Started</a>
-        </div>
-      <!-- </div> -->
-      <div class="col-12">
-        <section id="featured-services" class="featured-services">
-          <div class="container" data-aos="fade-up">
-            <div class="row">
-              <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
-                <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
-                  <div class="row">
-                    <div class="col-12">
-                        <img src="assets/img/logosp.png" width="50%" style="margin-right: 25%;margin-left: 25%">
-                    </div>
-
-                    <h4 class="title" style="text-align: center;"><a href="">CV. SUMBER PANGAN</a></h4>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
-                <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
-                  <div class="row">
-                    <div class="col-12">
-                        <img src="assets/img/logosps.png" width="50%" style="margin-right: 25%;margin-left: 25%">
-                    </div>
-
-                    <h4 class="title" style="text-align: center;"><a href="">PT. SUMBER PANGAN</a></h4>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
-                <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
-                  <div class="row">
-                    <div class="col-12">
-                        <img src="assets/img/logosip.png" width="50%" style="margin-right: 25%;margin-left: 25%">
-                    </div>
-
-                    <h4 class="title" style="text-align: center;"><a href="">CV. SURYA INTI PANGAN</a></h4>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-
-          </div>
-        </section>
-      </div>
-      </div>
-    </div>
-  </section><!-- End Hero -->
-
-  <footer id="footer">
-
-
-
-    <div class="container py-4">
-      <div class="copyright">
-        &copy; Copyright <strong><span>BizLand</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bizland-bootstrap-business-template/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
-
-   <section id="topbar" class="d-flex align-items-center">
-    <div class="container d-flex justify-content-center justify-content-md-between">
-      <div class="contact-info d-flex align-items-center">
-      </div>
-    </div>
-  </section>
+  <section class="content" style="height: auto">
+    @yield('isi')
+</section>
   <!-- Vendor JS Files -->
-  <script src="{{ url('public/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
-  <script src="{{ url('public/assets/vendor/aos/aos.js') }}"></script>
-  <script src="{{ url('public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ url('public/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-  <script src="{{ url('public/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-  <script src="{{ url('public/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-  <script src="{{ url('public/assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
-  <script src="{{ url('public/assets/vendor/php-email-form/validate.js') }}"></script>
+  <script src="{{ url('public/holding/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+  <script src="{{ url('public/holding/assets/vendor/aos/aos.js') }}"></script>
+  <script src="{{ url('public/holding/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ url('public/holding/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+  <script src="{{ url('public/holding/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ url('public/holding/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ url('public/holding/assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
+  <script src="{{ url('public/holding/assets/vendor/php-email-form/validate.js') }}"></script>
 
   <!-- Template Main JS File -->
-  <script src="{{ url('public/assets/js/main.js') }}"></script>
+  <script src="{{ url('public/holding/assets/js/main.js') }}"></script>
 
 </body>
 
